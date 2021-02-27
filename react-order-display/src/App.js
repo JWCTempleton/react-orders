d8bf8e19b7dbdf1e724aaf7b6339cda86fd5417b
Jacob Templeton <jwt3@stmarys-ca.edu>
2021-02-26 18:19:00 -0800

    added scrolling element around order display

--- react-order-display/src/App.js
+++ react-order-display/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
 
 import './App.css';
 
@@ -34,7 +35,9 @@ class App extends Component {
       <div className="tc" >
         <h1 className='tc f1 order-header'>Order List</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList orders={filteredOrders} />
+        <Scroll>
+          <CardList orders={filteredOrders} />
+        </Scroll>
       </div>
     );
   }
--- react-order-display/src/SearchBox.js
+++ react-order-display/src/SearchBox.js
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBox = ({ searchfield, searchChange }) => {
     return (
         <input
-            className="pa3 ba b--washed-green bg-lightest-blue"
+            className="pa3 ba mb3 b--washed-green bg-lightest-blue"
             type="search"
             placeholder="search orders"
             onChange={searchChange}
